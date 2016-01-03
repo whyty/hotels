c8517e7424487a9b5e5e7f8978cc09e59f8ed106
Victor Albulescu <albulescu.victor.alexandru@gmail.com>
2016-01-03 15:00:00 +0200

    Changed verification condition into add section for hotel, theme, airport, etc

--- application/views/admin/addHotel.php
+++ application/views/admin/addHotel.php
@@ -1,19 +1,19 @@
 <?php require_once(ROOT . DS . 'application' . DS . 'views' . DS . 'admin' . DS . 'topMenu.php' ); ?>
 <div class="col-lg-6">
 	<form role="form" action="/admin/insertHotel" method="post">
-		<input type="hidden" name="id" value="<?php echo isset($hotel) ? $hotel['id'] : '' ?>" />
+		<input type="hidden" name="id" value="<?php echo $hotel ? $hotel['id'] : '' ?>" />
 		<div class="form-group input-group">
 			<span class="input-group-addon">Name</span>
-			<input type="text" class="form-control" placeholder="Hotel Name" value="<?php echo isset($hotel) ? $hotel['name'] : '' ?>" name="name" required>
+			<input type="text" class="form-control" placeholder="Hotel Name" value="<?php echo $hotel ? $hotel['name'] : '' ?>" name="name" required>
 		</div>
 
 		<div class="form-group input-group">
 			<span class="input-group-addon">Stars</span>
-			<input type="number" class="form-control" placeholder="Stars" value="<?php echo isset($hotel) ? $hotel['stars'] : '' ?>" name="stars" required>
+			<input type="number" class="form-control" placeholder="Stars" value="<?php echo $hotel? $hotel['stars'] : '' ?>" name="stars" required>
 		</div>
 		<div class="form-group input-group">
 			<span class="input-group-addon">Meal</span>
-			<input type="text" class="form-control" placeholder="Meal" value="<?php echo isset($hotel) ? $hotel['meal'] : '' ?>" name="meal">
+			<input type="text" class="form-control" placeholder="Meal" value="<?php echo $hotel ? $hotel['meal'] : '' ?>" name="meal">
 		</div>
 		<?php if(count($intervals) > 0) :?>
 		<div class="form-group">
--- application/views/admin/addTheme.php
+++ application/views/admin/addTheme.php
@@ -1,10 +1,10 @@
 <?php require_once(ROOT . DS . 'application' . DS . 'views' . DS . 'admin' . DS . 'topMenu.php' ); ?>
 <div class="col-lg-6">
 	<form role="form" action="/admin/insertTheme" method="post">
-		<input type="hidden" name="id" value="<?php echo isset($theme) ? $theme['id'] : '' ?>" />
+		<input type="hidden" name="id" value="<?php echo $theme ? $theme['id'] : '' ?>" />
 		<div class="form-group input-group">
 			<span class="input-group-addon">Name</span>
-			<input type="text" class="form-control" placeholder="Theme Name" value="<?php echo isset( $theme) ?  $theme['name'] : '' ?>" name="name" required>
+			<input type="text" class="form-control" placeholder="Theme Name" value="<?php echo theme ?  $theme['name'] : '' ?>" name="name" required>
 		</div>
 		<button type="submit" class="btn btn-primary pull-right">Save theme</button>
 	</form>
